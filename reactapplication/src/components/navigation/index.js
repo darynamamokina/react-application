b374c3cae00de0112454de0c8982e170351f25b9
darynamamokina <116071359+darynamamokina@users.noreply.github.com>
2022-11-15 12:55:40 +0200

    message

--- reactapplication/src/components/navigation/index.js
+++ reactapplication/src/components/navigation/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import './style.css';
 
-
 function Navigation () {
- return (
-  <nav>
-   <div className="menu-title">Milton</div>
-   <hr />
-   <div className="menu-text">Denali is a simple responsive blog 
-   template. Easily add new posts using the Editor or change 
-   layout and design using the Designer.
-   </div>
-   <hr />
-   <ul className="menu">
-    <li>home</li>
-    <li>about</li>
-    <li>contact</li>
-   </ul>
-   <hr />
-   <div className="logo"></div>
-  </nav>
- );
+    return ( 
+        <nav>
+            <div>
+                <div className="menu">
+                    <div className="text_menu">
+                      <p>RS-1200 Prototype 35</p>
+                      <p>Free Responsive Template</p>
+                    </div>
+                    <div>
+                        <p className="email">info@domain.com</p>
+                        <p className="phone">+xx xxx xxxxxxxxxx</p>
+                    </div>
+                </div>
+            </div>
+            <div className="menu_burgers">
+                
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
