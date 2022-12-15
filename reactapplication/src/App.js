19c7896e8f152bf5f8109db218f0d9bbae7e0445
darynamamokina <116071359+darynamamokina@users.noreply.github.com>
2022-12-15 21:56:57 +0200

    test

--- reactapplication/src/App.js
+++ reactapplication/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation';
 import Content from './components/content';
+import NotFoundPage from './components/notFoundPage';
+import About from './components/about';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <Navigation />
-      <Content />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Content />} >
+            <Route path='/card/:cardId' element={<Content />} />
+          </Route>
+          <Route path='/about' element={<About />} />
+
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
--- reactapplication/src/components/content/index.js
+++ reactapplication/src/components/content/index.js
@@ -1,38 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './style.css';
 import Card from '../card';
-import image1 from '../imgs/1.jpg';
+import mockData from '../mock/data.json';
+import { NavLink, useParams } from "react-router-dom";
 
 
 function Content () {
- const cards = [
-  {
-   image: image1,
-   title: 'Coffee variety macchiato, as organic ut variety caffeine americano',
-   description: 'Saucer, crema carajillo, bar, mocha medium, latte cappuccino and espresso acerbic to go. Coffee, irish foam turkish coffee blue mountain seasonal. Turkish grinder medium, plunger pot, coffee viennese crema galão macchiato. Filter, cinnamon, caffeine in, cortado, plunger pot decaffeinated cinnamon lungo con panna milk',
-   date: 'June 25, 2020',
-   tag: 'Travel',
-  },
-  {
-   image: image1,
-   title: 'Coffee variety macchiato, as organic ut variety caffeine americano',
-   description: 'Saucer, crema carajillo, bar, mocha medium, latte cappuccino and espresso acerbic to go. Coffee, irish foam turkish coffee blue mountain seasonal. Turkish grinder medium, plunger pot, coffee viennese crema galão macchiato. Filter, cinnamon, caffeine in, cortado, plunger pot decaffeinated cinnamon lungo con panna milk',
-   date: 'June 25, 2020',
-   tag: 'Travel',
-  }
- ];
+  const cards = mockData;
+  const [tag, setTag] = useState();
+  console.log('tag:', tag);
 
- return (
-  <div className="content-container">
-   {cards.map(card => <Card
-    image={card.image}
-    title={card.title}
-    description={card.description}
-    date={card.date}
-    tag={card.tag}
-   />)}
-  </div>
- );
+  const classNames = ['selected-tag'];
+  if (tag) {
+    classNames.push('show');
+  }
+
+  const [selectedCard, setSelectedCard] = useState();
+  console.log('selectedCard:', selectedCard);
+
+  const { cardId } = useParams();
+  useEffect(() => {
+    setSelectedCard(
+      cards.find(card => card.id == cardId)
+    );
+  }, [cardId]);
+  return (
+    <div className="content-cont">
+      <div className={classNames.join(' ')}>{tag}</div>
+      {cards
+        .filter(card => {
+          if (tag) {
+            return card.tag === tag;
+          }
+
+          if (selectedCard) {
+            return card.id === selectedCard.id;
+          }
+
+          return true;
+        })
+        .map(card => {
+
+          return (
+            <NavLink to={`/card/${card.id}`}>
+              <Card
+                id={card.id}
+                isArticleVisible={selectedCard && card.id === selectedCard.id}
+                image={card.image}
+                title={card.title}
+                description={card.description}
+                date={card.date}
+                tag={card.tag}
+                fullPage={card.fullPage}
+                setSelectedTag={setTag}
+                setSelectedCard={() => setSelectedCard(card)}
+              /></NavLink>);
+        })}
+      <NavLink to='/'>
+        <div
+          className="return-all-posts"
+          onClick={() => { setTag(); setSelectedCard(); }}
+        >All posts</div>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Content;
